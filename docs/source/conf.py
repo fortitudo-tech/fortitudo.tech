22b583ae7d2ac2d71328777a1c0afc12460a30a0
Anton Vorobets <admin@fortitudo.tech>
2022-02-20 12:55:17 +0100

    Added searchbox to docs website

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ typehints_document_rtype = True
 html_show_sourcelink = False
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
