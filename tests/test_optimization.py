0fdc86e1c0e981a6cab34f90ce698033d9559526
Anton Vorobets <admin@fortitudo.tech>
2022-08-03 10:55:21 +0200

    Significant test_optimization refactor

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -16,13 +16,13 @@
 
 import numpy as np
 import pytest
-from context import R, MeanCVaR, cvar_options, MeanVariance
+from context import R, MeanCVaR, cvar_options, MeanVariance, covariance_matrix
 
 R = R.values
 S, I = R.shape
 mean = np.mean(R, axis=0)
-covariance_matrix = np.cov(R, rowvar=False)
-tol = 1e-7
+cov_matrix = np.cov(R, rowvar=False)
+
 G = -np.eye(I)
 h = np.zeros(I)
 A = np.zeros((2, I))
@@ -30,37 +30,34 @@ A[0, 6] = 1
 A[1, :] = 1
 b = np.array([0.1, 1])
 
-
-def test_long_short():
-    np.random.seed(1)
-    p = np.random.randint(1, S, (S, 1))
-    p = p / np.sum(p)
-    opt = MeanCVaR(R, p=p)
-    min_risk_pf = opt.efficient_portfolio()
-    target_return_pf = opt.efficient_portfolio(0.06)
-    assert np.abs(np.sum(min_risk_pf) - 1) <= tol
-    assert np.abs(np.sum(target_return_pf) - 1) <= tol
-    assert np.abs(p.T @ R @ target_return_pf - 0.06) <= tol
-    with pytest.raises(ValueError):
-        opt.efficient_frontier()
+tol = 1e-7
+np.random.seed(1)
+p = np.random.randint(1, S, (S, 1))
+p = p / np.sum(p)
+mean_random = R.T @ p
+cov_matrix_random = covariance_matrix(R, p).values
 
 
-def test_long_short_variance():
-    opt = MeanVariance(mean, covariance_matrix)
-    min_risk_pf = opt.efficient_portfolio()
-    target_return_pf = opt.efficient_portfolio(0.06)
-    assert np.abs(np.sum(min_risk_pf) - 1) <= tol
-    assert np.abs(np.sum(target_return_pf) - 1) <= tol
-    assert np.abs(mean @ target_return_pf - 0.06) <= tol
-    with pytest.raises(ValueError):
-        opt.efficient_frontier()
-
-
-opt0 = MeanCVaR(R, G=G, h=h, options={'demean': False})
-opt1 = MeanVariance(mean, covariance_matrix, G=G, h=h)
+opt0 = MeanCVaR(R, p=p)
+opt1 = MeanVariance(mean_random, cov_matrix_random)
 
 
 @pytest.mark.parametrize("opt", [(opt0), (opt1)])
+def test_long_short(opt):
+    min_risk_pf = opt.efficient_portfolio()
+    target_return_pf = opt.efficient_portfolio(0.06)
+    assert np.abs(np.sum(min_risk_pf) - 1) <= tol
+    assert np.abs(np.sum(target_return_pf) - 1) <= tol
+    assert np.abs(mean_random.T @ target_return_pf - 0.06) <= tol
+    with pytest.raises(ValueError):
+        opt.efficient_frontier()
+
+
+opt2 = MeanCVaR(R, G=G, h=h, options={'demean': False})
+opt3 = MeanVariance(mean, cov_matrix, G=G, h=h)
+
+
+@pytest.mark.parametrize("opt", [(opt2), (opt3)])
 def test_long_only(opt):
     min_risk_lo = opt.efficient_portfolio()
     target_return_lo = opt.efficient_portfolio(0.06)
@@ -72,11 +69,11 @@ def test_long_only(opt):
     assert np.max(np.abs(frontier_lo[:, 0] - min_risk_lo[:, 0])) <= tol
 
 
-opt2 = MeanCVaR(R, A, b, G, h)
-opt3 = MeanVariance(mean, covariance_matrix, A, b, G, h)
+opt4 = MeanCVaR(R, A, b, G, h)
+opt5 = MeanVariance(mean, cov_matrix, A, b, G, h)
 
 
-@pytest.mark.parametrize("opt", [(opt2), (opt3)])
+@pytest.mark.parametrize("opt", [(opt4), (opt5)])
 def test_equality_constraint(opt):
     min_risk_eq = opt.efficient_portfolio()
     target_return_eq = opt.efficient_portfolio(0.06)
@@ -92,8 +89,8 @@ def test_equality_constraint(opt):
 
 def test_options():
     cvar_options['demean'] = False
-    opt4 = MeanCVaR(R, A, b, G, h)
-    assert opt4._demean is False
+    opt6 = MeanCVaR(R, A, b, G, h)
+    assert opt6._demean is False
     with pytest.raises(ValueError):
         MeanCVaR(R, options={'demean': 'X'})
     with pytest.raises(ValueError):
@@ -112,13 +109,13 @@ def test_infeasible_constraints():
     with pytest.raises(ValueError):
         MeanCVaR(R, G=G_infeasible, h=h_infeasible)
     with pytest.raises(ValueError):
-        MeanVariance(mean, covariance_matrix, G=G_infeasible, h=h_infeasible)
+        MeanVariance(mean, cov_matrix, G=G_infeasible, h=h_infeasible)
 
 
 def test_alpha_parameter():
-    assert opt2._alpha == 0.95
-    opt4 = MeanCVaR(R, A, b, G, h, alpha=0.9)
-    assert opt4._alpha == 0.9
+    assert opt0._alpha == 0.95
+    opt7 = MeanCVaR(R, A, b, G, h, alpha=0.9)
+    assert opt7._alpha == 0.9
     with pytest.raises(ValueError):
         MeanCVaR(R, A, b, G, h, alpha=1.1)
     with pytest.raises(ValueError):
