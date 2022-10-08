a159e1f954239a794d7a2fa234d83bbffba238eb
Anton Vorobets <admin@fortitudo.tech>
2022-10-08 15:09:44 +0200

    Changed maxiter to maxfun due to deprecation warning

--- fortitudo/tech/entropy_pooling.py
+++ fortitudo/tech/entropy_pooling.py
@@ -50,7 +50,7 @@ def entropy_pooling(
         rhs = np.vstack((b, h))
         solution = minimize(
             _dual_inequality, x0=np.zeros(len_bh), args=(log_p, lhs, rhs),
-            method='TNC', jac=True, bounds=bounds, options={'maxiter': 10000})
+            method='TNC', jac=True, bounds=bounds, options={'maxfun': 10000})
     q = np.exp(log_p - 1 - lhs.T @ solution.x[:, np.newaxis])
     return q
 
