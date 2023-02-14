a12c6a5acfd3b3031428848d394f3e881777de16
Anton Vorobets <82604278+antonvorobets@users.noreply.github.com>
2023-02-14 20:02:04 +0100

    v0.8.2 (#53)
    * License update
    
    * Docs / README update
    
    * Requirements update
    
    * Poetry update

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,5 @@
 # fortitudo.tech - Novel Investment Technologies.
-# Copyright (C) 2021-2022 Fortitudo Technologies.
+# Copyright (C) 2021-2023 Fortitudo Technologies.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
